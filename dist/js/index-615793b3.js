1d6126dc317acfa211415e9ac4344e7c0efdcc30
liangxi <1066039213@qq.com>
2025-09-04 07:28:15 +0800

    提交dist文件

---
+++ dist/js/index-615793b3.js
@@ -0,0 +1,1 @@
+import{h as a}from"./index-5de9982a.js";function o(t,r){return a({url:"/public/ask-code",params:{email:t,type:r},method:"get"})}export{o as s};
---
+++ dist/js/index-71ccabcd.js
@@ -0,0 +1,1 @@
+import{h as t}from"./index-5de9982a.js";function o(){return t.get("/tag/list",{method:"get"})}export{o as t};
